85c12ba1577d9ffcf4e0989d845e9c1d94ae75cf
Pothamsetti Sai Srujan Reddy <157013684+SaiSrujanReddyP@users.noreply.github.com>
2025-03-13 21:57:37 +0530

    Create 13-03-2025.cpp

---
+++ 13-03-2025.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        int n = val.size();
+        vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+        
+        for (int i = 1; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (wt[i - 1] <= w) {
+                    dp[i][w] = max(dp[i - 1][w], val[i - 1] + dp[i - 1][w - wt[i - 1]]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+        return dp[n][W];
+    }
+};
